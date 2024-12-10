8ab6daf90393a1b9893cfe8c8d280a4e84f33944
Matteo Badini <matteo.badini@gellify.com>
2024-12-10 21:39:57 +0100

    feat: add i18n

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { Toaster } from "~/components/ui/sonner";
+import { I18nProviderClient } from "~/locales/client";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,17 +17,23 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
+  params,
   children,
 }: {
+  params: Promise<{ locale: string }>;
   children: React.ReactNode;
 }) {
+  const { locale } = await params;
+
   return (
     <ClerkProvider dynamic>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={`font-sans ${inter.variable}`}>
-          {children}
-          <Toaster />
+          <I18nProviderClient locale={locale}>
+            {children}
+            <Toaster />
+          </I18nProviderClient>
         </body>
       </html>
     </ClerkProvider>
