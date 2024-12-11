354412e25ad0afd019599ff14e3a23fc4305a7cc
Matteo Badini <matteo.badini@gellify.com>
2024-12-11 09:47:06 +0100

    chore: update dep

--- src/server/db/migrations/0000_calm_luckman.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "acme_post" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
-	"updated_at" timestamp with time zone NOT NULL,
-	"title" varchar(256) NOT NULL,
-	"content" varchar(256) NOT NULL
-);
