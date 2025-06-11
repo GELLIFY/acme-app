ed525a08ecf7411d81df5f4a053ec6dfd72234fc
Matteo Badini <matteo.badini@gellify.com>
2025-06-11 10:36:50 +0200

    fix: caching

--- src/server/services/todo-service.ts
+++ src/server/services/todo-service.ts
@@ -1,21 +1,12 @@
-import { unstable_cache } from "next/cache";
+"server-only";
 
 import { shuffleTodos } from "../domain/todo/helpers";
 import { getTodosQuery } from "../domain/todo/queries";
 
 export async function getTodos() {
-  return unstable_cache(
-    async () => {
-      const todos = await getTodosQuery();
+  const todos = await getTodosQuery();
 
-      // NOTE: do whatever you want here, map, aggregate filter...
-      // result will be cached and typesafety preserved
-      return shuffleTodos(todos);
-    },
-    ["todos"],
-    {
-      tags: ["todos"],
-      revalidate: 3600,
-    },
-  )();
+  // NOTE: do whatever you want here, map, aggregate filter...
+  // result will be cached and typesafety preserved
+  return shuffleTodos(todos);
 }
