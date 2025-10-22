b3831e2fe07b5835ecba1335d87c4ff08514623e
Matteo Badini <matteo.badini@gellify.com>
2025-10-22 15:58:30 +0200

    chore: remove log

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "drizzle-kit";
 
 import { env } from "@/env";
 
-console.log("env", env.DATABASE_URL, process.env.DATABASE_URL);
-
 export default defineConfig({
   out: "./src/server/db/migrations",
   schema: "./src/server/db/schema",
