465b2db4a3afe8cdaac5a0bbf96be9506b78112d
Matteo Badini <matteo.badini@gellify.com>
2025-10-24 12:47:47 +0200

    WIP

--- src/shared/helpers/better-auth/auth.ts
+++ src/shared/helpers/better-auth/auth.ts
@@ -1,22 +1,25 @@
+import { instrumentBetterAuth } from "@kubiks/otel-better-auth";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { db } from "@/server/db";
 
-export const auth = betterAuth({
-  advanced: {
-    database: {
-      generateId: false,
+export const auth = instrumentBetterAuth(
+  betterAuth({
+    advanced: {
+      database: {
+        generateId: false,
+      },
     },
-  },
-  database: drizzleAdapter(db, {
-    provider: "pg", // or "mysql", "sqlite"
+    database: drizzleAdapter(db, {
+      provider: "pg", // or "mysql", "sqlite"
+    }),
+    emailAndPassword: {
+      enabled: true,
+      async sendResetPassword(_data, _request) {
+        // Send an email to the user with a link to reset their password
+      },
+    },
+    plugins: [nextCookies()],
   }),
-  emailAndPassword: {
-    enabled: true,
-    async sendResetPassword(_data, _request) {
-      // Send an email to the user with a link to reset their password
-    },
-  },
-  plugins: [nextCookies()],
-});
+);
