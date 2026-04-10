ac241481acc201b8db4cbcdc48822b15d8b04d55
matteobadini <matteo.badini@gellify.com>
2026-04-10 10:18:59 +0200

    feat: add perf budget

---
+++ lighthouserc.cjs
@@ -0,0 +1,23 @@
+const config = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/', 'http://localhost:3000/blog'],
+      startServerCommand: 'pnpm run start',
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
+        'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
+        'total-blocking-time': ['error', { maxNumericValue: 200 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 1500 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
+
+module.exports = config
